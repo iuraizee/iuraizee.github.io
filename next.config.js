1df6999f90fbf749d0f618a829a93c85855ada81
Irfan Uraizee <iuraizee@gmail.com>
2020-05-16 17:12:22 -0400

    update static deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const withSass = require('@zeit/next-sass')
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = withSass({
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? '/' : '',
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+
   webpack(config) {
     return config;
   }
